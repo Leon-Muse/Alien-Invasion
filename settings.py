c87eb299f9ef4d6db9164e650529e73632bedbb5
Omar Montoya <heyitsomar18@gmail.com>
2022-08-25 21:38:59 -0700

    git commit

--- settings.py
+++ settings.py
@@ -1,6 +1,9 @@
 class Settings:
-    
+    #A class to store all the settings for the game
+
     def __init__(self):
+        #intialize the game's settings
+        #screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (100,100,230)
+        self.bg_color = (230,230,230)
